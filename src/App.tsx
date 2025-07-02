6db8f0fde481d944dba20a1032af5bd7b1d0f935
JavierAQR <nonamesteam123@gmail.com>
2025-07-01 23:23:57 -0500

    feat: React Router agregado

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
+import { Navigate, Route, Routes } from 'react-router'
 import './App.css'
 import Gallery from './pages/Gallery'
+import { BrowserRouter as Router } from 'react-router'
 
 function App() {
 
   return (
     <>
+    <Router>
+      <Routes>
+        <Route index element={<Navigate to="gallery" replace />} />
+        <Route path="gallery" element={<Gallery />} />
+      </Routes>
+    </Router>
     <Gallery/>
     </>
   )
